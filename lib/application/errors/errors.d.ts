a8b3c08cc09ed2077f899bbf91eb1de69250ea0a
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2022-07-13 19:06:06 +0200

    Update

--- lib/application/errors/errors.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class MissingParentSpanError extends Error {
-    constructor(parentSpanName: string);
-}
-export declare class SpanAlreadyExistsError extends Error {
-    constructor(spanName: string);
-}
-export declare class MissingSpanNameError extends Error {
-    constructor();
-}
